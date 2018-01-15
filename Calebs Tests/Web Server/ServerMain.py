8bb2814ace6d1ba863a38070d6e30ab08c9d91c0
cboy116 <calebjwiebolt@gmail.com>
2018-01-14 20:14:38 -0600

    More Web Server

--- Calebs Tests/Web Server/ServerMain.py
+++ Calebs Tests/Web Server/ServerMain.py
@@ -1,6 +1,7 @@
 import tornado.web
 import tornado.ioloop
 import tornado.websocket
+import os
 
 import Handlers as WH
 
@@ -8,16 +9,21 @@ def CheckEvents():
     pass
 
 def ServerCreate():
+    settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), "static")
+    }
+
     app = tornado.web.Application([
-        (r"/", WH.defaultHandler),
-        (r"/eventHandler",WH.EventSocket)
-    ])
+        (r"/", WH.DefaultHandler),
+        (r"/EventHandler", WH.EventSocket)
+    ],**settings)
+    
     app.listen(8888)
 
     ioLoop = tornado.ioloop.IOLoop.current()
 
     #run this function every 1000ms
-    ioLoop.PeriodicCallback(CheckEvents,1000)
+    #ioLoop.PeriodicCallback(CheckEvents,1000)
 
     #start processing loop
     ioLoop.start()
--- Calebs Tests/Web Server/static/eventHandler.js
+++ Calebs Tests/Web Server/static/eventHandler.js
@@ -1,6 +1,6 @@
 var webSocket;
 
-webSocket = new WebSocket("ws://localhost:8888/eventHandler")
+webSocket = new WebSocket("ws://localhost:8888/EventHandler")
 
 webSocket.onmessage = function(e){
     alert(e.data);
