017bd2a1e946fc683e6bc88842f0c0da022397cd
cboy116 <calebjwiebolt@gmail.com>
2018-01-25 09:09:27 -0600

    More Work

--- Calebs Tests/Web Server/static/eventHandler.js
+++ Calebs Tests/Web Server/static/eventHandler.js
@@ -15,7 +15,29 @@ class Popout{
     constructor(Key,ID){
         this.key = Key;
         this.id = ID;
+        this.dur = 5.0;
         
+    }
+    
+    fire(value=null,duration=5.0){
+        this.dur = duration;
+        
+        
+    }
+    
+    animation(){
+        $(this.id).animate({
+            left: 0;
+        },
+        "slow"
+        )
+    }
+    /*Getters and Setters*/
+    get id(){
+        return this.id;
+    }
+    get key(){
+        return this.key;
     }
     
 }
