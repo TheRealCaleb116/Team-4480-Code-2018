38be21ae43e3ff44c2d2b47220e53dc85c2fc06e
bb20basketball <bb20basketball@gmail.com>
2018-02-01 22:10:33 -0600

    simple test

--- Robot Code/robot.py
+++ Robot Code/robot.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
-# Python3 Robot code: "Unititled"
-# 2018 - 2018, 4480 "Forty48ie" "UC-Botics"
-#
-
 import wpilib
-import wpilib.buttons
 import pathfinder as pf
-import random
-from robotpy_ext.autonomous import AutonomousModeSelector
-from robotpy_ext.common_drivers import units, navx
-import networktables
 from wpilib.drive import DifferentialDrive
 import wpilib.drive
 import math
@@ -21,21 +12,17 @@ class MyRobot(wpilib.IterativeRobot):
 
 
         #Motors
-        self.leftMotorInput = wpilib.Talon(1) #  AEN
-        self.rightMotorInput = wpilib.Talon(2) # AEN
+        self.leftMotorInput = wpilib.Talon(1)
+        self.rightMotorInput = wpilib.Talon(2)
 
         self.drive = wpilib.drive.DifferentialDrive(self.leftMotorInput, self.rightMotorInput)
 
 
         #Inputs
-        self.xboxController = wpilib.Joystick(0)
-        self.xboxAbutton = wpilib.buttons.JoystickButton(self.xboxController, 1)
-        self.xboxBbutton = wpilib.buttons.JoystickButton(self.xboxController, 2)
-        self.xboxYbutton = wpilib.buttons.JoystickButton(self.xboxController, 4)
+        self.controller = wpilib.Joystick(0)
 
 
         # pathfinder stuff
-
         points = [
             pf.Waypoint(-4, -1, math.radians(-45.0)),
             pf.Waypoint(-2, -2, 0),
@@ -51,30 +38,24 @@ class MyRobot(wpilib.IterativeRobot):
         left = modifier.getLeftTrajectory()
         right = modifier.getRightTrajectory()
 
-        self.left_follower = pf.followers.EncoderFollower(left)
-        self.right_follower = pf.followers.EncoderFollower(right)
+        self.left_follower = pf.followers.DistanceFollower(left)
+        self.right_follower = pf.followers.DistanceFollower(right)
 
-        self.left_follower.configureEncoder(2, 1000, 5.75)
-        self.left_follower.configurePIDVA(1.0, 0.0, 0.0, 1 / 1.7, 0)
+        #self.left_follower.configureEncoder(2, 1000, 5.75)
+        self.left_follower.configurePIDVA(5.0, 0.0, 0.0, 1 / 20, 0)
+
+        #self.right_follower.configureEncoder(2, 1000, 5.75)
+        self.right_follower.configurePIDVA(5.0, 0.0, 0.0, 1 / 20, 0)
 
         # simulating an encoder position ticks
-        self.counter = 100
-
-        self.components = {
-            'drive': self.drive
-        }
-        self.automodes = AutonomousModeSelector('autonomous', self.components)
-
-
-    def autonomousPeriodic(self):
-        self.automodes.run()
-
+        self.counter = 1
 
     def teleopPeriodic(self):
-        self.counter+=100
+
+        self.counter+=1
         print (self.left_follower.calculate(self.counter))
 
-        self.drive.tankDrive(self.xboxController.getY(), self.xboxController.getRawAxis(5))
+        self.drive.tankDrive(self.controller.getY(), self.controller.getRawAxis(5))
 
 
 if __name__ == "__main__":
